de5f2552a861165b2bafd0090ef54830276b68d9
Jörn Horstmann <git@jhorstmann.net>
2023-07-05 14:05:12 +0200

    Test for aggregations with fadd_fast

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -6,12 +6,43 @@ pub fn arithmetic_add(a: &[f32; 32], b: &[f32; 32], c: &mut [f32; 32]) {
         });
 }
 
-pub fn arithmetic_sum_iter(a: &[f32; 32]) -> f32 {
+pub fn arithmetic_sum_u32(a: &[u32; 32]) -> u32 {
     a.iter().copied().sum()
 }
 
-pub fn arithmetic_sum_fast(a: &[f32; 32]) -> f32 {
+pub fn arithmetic_sum_f32(a: &[f32; 32]) -> f32 {
+    a.iter().copied().sum()
+}
+
+/// "fun, safe math"
+pub fn arithmetic_sum_unsafe(a: &[f32; 32]) -> f32 {
     a.iter()
         .copied()
         .fold(0.0, |a, x| unsafe { std::intrinsics::fadd_fast(a, x) })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::arithmetic::{arithmetic_sum_f32, arithmetic_sum_unsafe};
+    use std::hint::black_box;
+
+    #[test]
+    fn test_sum_f32() {
+        assert_eq!(f32::MAX + f32::MIN + 0.25, 0.25);
+        assert_eq!(f32::MAX + 0.25 + f32::MIN, 0.0);
+
+        let input = black_box({
+            let mut tmp = [0_f32; 32];
+            tmp[0] = f32::MAX;
+            tmp[1] = 0.25;
+            tmp[8] = f32::MIN;
+            tmp
+        });
+
+        assert_eq!(arithmetic_sum_f32(&input), 0.0);
+        assert_eq!(
+            arithmetic_sum_unsafe(&input),
+            if !cfg!(debug_assertions) { 0.25 } else { 0.0 }
+        );
+    }
 }
